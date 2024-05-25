a81abf4efcff498d55cdee5d474ad48efa53aa1a
Mamani Xavier <91898634+xaviermaxd@users.noreply.github.com>
2024-05-25 15:41:27 -0500

    Agregar archivos de proyecto.

---
+++ lab10/Controllers/clientesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lab10.Models;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+
+namespace lab10.Controllers
+{
+    public class clientesController : Controller
+    {
+        private NeptunoEntities db = new NeptunoEntities();
+
+        // GET: clientes
+        public ActionResult Index()
+        {
+            return View(db.clientes.Where(c => c.Enable == true).ToList());
+        }
+
+        // GET: clientes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clientes clientes = db.clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // GET: clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idCliente,NombreCompañia,NombreContacto,CargoContacto,Direccion,Ciudad,Region,CodPostal,Pais,Telefono,Fax")] clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                clientes.Enable = true;
+                db.clientes.Add(clientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientes);
+        }
+
+
+        // GET: clientes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clientes clientes = db.clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: clientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idCliente,NombreCompañia,NombreContacto,CargoContacto,Direccion,Ciudad,Region,CodPostal,Pais,Telefono,Fax,Enable")] clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientes);
+        }
+
+        // GET: clientes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            clientes clientes = db.clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            clientes clientes = db.clientes.Find(id);
+            if (clientes != null)
+            {
+                clientes.Enable = false;
+                db.Entry(clientes).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
